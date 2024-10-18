1b7ff9ef1788a83a871a6c2de01011b15b9a5324
Prince Dayma <115869150+Princedayma@users.noreply.github.com>
2024-10-18 13:00:16 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,20 @@
 import { NextResponse, NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname;
+// export function middleware(request: NextRequest) {
+//   const path = request.nextUrl.pathname;
 
-  const isPublic = path === '/login' || path === '/signup';
+//   const isPublic = path === '/login' || path === '/signup';
 
-  const token = request.cookies.get('token')?.value || '';
+//   const token = request.cookies.get('token')?.value || '';
 
-  // if (isPublic && token) {
-  //   return NextResponse.redirect(new URL('/', request.nextUrl));
-  // }
+//   // if (isPublic && token) {
+//   //   return NextResponse.redirect(new URL('/', request.nextUrl));
+//   // }
 
-  if (!isPublic && !token) {
-    return NextResponse.redirect(new URL('/login', request.nextUrl));
-  }
-}
+//   if (!isPublic && !token) {
+//     return NextResponse.redirect(new URL('/login', request.nextUrl));
+//   }
+// }
 
 export const config = {
   matcher: [
