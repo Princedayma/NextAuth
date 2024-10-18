8e05983d2da0585c508d46a4091cdd6bf4ba8d9a
Prince Dayma <115869150+Princedayma@users.noreply.github.com>
2024-10-18 17:40:24 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,17 +3,33 @@ import { NextResponse, NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
 
-  // If someone tries to access "/", redirect them to "/SignUp"
-  if (path === '/') {
-    return NextResponse.redirect(new URL('/SignUp', request.nextUrl));
+  const isPublic = path === '/login' || path === '/signup';
+  const token = request.cookies.get('token')?.value || '';
+
+  // Allow access to the public pages
+  if (isPublic) {
+    // If user is trying to access signup or login and already has a token, redirect them to the home page
+    if (path == '/') {
+      return NextResponse.redirect(new URL('/SignUp', request.nextUrl));
+    }
+    // Allow normal access to public pages
+    return NextResponse.next();
   }
 
-  // Allow all other requests to pass through
+  // For non-public pages, if token is not present, redirect to signup
+  // if (!token) {
+  //   return NextResponse.redirect(new URL('/signup', request.nextUrl));
+  // }
+
+  // Otherwise, allow access to protected routes
   return NextResponse.next();
 }
 
 export const config = {
   matcher: [
-    '/',  // This will match the root path
+    '/',
+    '/profile',
+    '/login',
+    '/signup',
   ],
 };
