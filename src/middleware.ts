3f01a96dd4f8eded1118b58897d12a8c8e452dcc
Prince Dayma <115869150+Princedayma@users.noreply.github.com>
2024-10-18 17:49:31 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,20 @@
 import { NextResponse, NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname;
+  // const path = request.nextUrl.pathname;
 
-  const isPublic = path === '/login' || path === '/signup';
-  const token = request.cookies.get('token')?.value || '';
+  // const isPublic = path === '/login' || path === '/signup';
+  // const token = request.cookies.get('token')?.value || '';
 
-  // Allow access to the public pages
-  if (isPublic) {
-    // If user is trying to access signup or login and already has a token, redirect them to the home page
-    if (path == '/') {
-      return NextResponse.redirect(new URL('/SignUp', request.nextUrl));
-    }
-    // Allow normal access to public pages
-    return NextResponse.next();
-  }
+  // // Allow access to the public pages
+  // if (isPublic) {
+  //   // If user is trying to access signup or login and already has a token, redirect them to the home page
+  //   if (path == '/') {
+  //     return NextResponse.redirect(new URL('/SignUp', request.nextUrl));
+  //   }
+  //   // Allow normal access to public pages
+  //   return NextResponse.next();
+  // }
 
   // For non-public pages, if token is not present, redirect to signup
   // if (!token) {
